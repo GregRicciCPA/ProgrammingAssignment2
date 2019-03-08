a341df7bde298e51a0c4a2a8c8c17e89192c306b
Greg Ricci <47262411+GregRicciCPA@users.noreply.github.com>
2019-03-08 15:57:37 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
-
+##
+## MakeCacheMatrix creates a unquie object matrix that can cache its inverse
+## Class project Lexical Scoping - Script1
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_m <- NULL
+  set <- function(y){
+    x <<-y
+    inv_m <<-NULL #local Eniv assingment
+  }
+  get <- function()x
+  setinverse <- function(inverse)inv_m <<- inverse #local Eniv assingment
+  getinverse <- function()inv_m
+  list(set=set,get=get,
+  setinverse=setinverse,
+  getinverse=getinverse)
 }
-
-
-## Write a short comment describing this function
-
+## Next code- Write a short comment describing this function
+##
+## CacheSolve returns the inverse of makeCacheMatrix function, and/or
+## computes the inverse then returns the value
+## Class project Lexical Scoping - Script2
+##
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv_m <-x$getinverse()
+  if(!is.null(inv_m)){
+## calculate ? Pause
+    message("Hold please, something realy cool is happening, getting Cached Inversed Matrix")
+  return(inv_m)
+    }else{
+  inv_m <- solve(x$get())
+  x$setinverse(inv_m)
+  
+  return(inv_m)
 }
+}
+                   
